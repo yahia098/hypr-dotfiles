3d961c93498d876c3951d5e7a287883e084a160a
yahia <90655976+yahia098@users.noreply.github.com>
2026-06-04 13:01:39 +0100

    Change file manager from 'dolphin' to 'kitty yazi'

--- .config/hypr/modules/programs.lua
+++ .config/hypr/modules/programs.lua
@@ -1,5 +1,5 @@
 Programs = {
 	terminal = "kitty",
-	fileManager = "dolphin",
+	fileManager = "kitty yazi",
 	menu = "wofi --show drun --define=drun --print_command=true --width 600 --height 300",
 }
