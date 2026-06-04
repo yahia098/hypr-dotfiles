4203d8baf0113dab906e35fa9dc08d6e595631ab
yahia098 <yahiaoui0982@gmail.com>
2026-06-04 13:03:44 +0100

    fixes

--- .config/hypr/modules/programs.lua
+++ .config/hypr/modules/programs.lua
@@ -1,5 +1,5 @@
 Programs = {
 	terminal = "kitty",
 	fileManager = "kitty yazi",
-	menu = "wofi --show drun --define=drun --print_command=true --width 600 --height 300",
+	menu = "wofi --show drun --define=drun --print_command=true --width 900 --height 600",
 }
