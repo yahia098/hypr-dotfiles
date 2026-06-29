65ac7d5acedac2b81cf4a731ab4e2e517e5c5ba4
yahia098 <yahiaoui0982@gmail.com>
2026-06-29 18:11:20 +0100

    removed pywall dependencies

--- .config/hypr/modules/autostart-env.lua
+++ .config/hypr/modules/autostart-env.lua
@@ -1,3 +1,6 @@
+-------------------
+---- AUTOSTART ----
+-------------------
 hl.on("hyprland.start", function()
 	hl.exec_cmd("$HOME/.config/hypr/scripts/prelock.sh")
 	hl.exec_cmd("hyprpaper")
@@ -5,5 +8,8 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("$HOME/.config/waybar/scripts/launch.sh")
 end)
 os.execute("$HOME/.config/waybar/scripts/launch.sh")
+-------------------------------
+---- ENVIRONMENT VARIABLES ----
+-------------------------------
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
--- .config/hypr/modules/monitors.lua
+++ .config/hypr/modules/monitors.lua
@@ -1,11 +1,14 @@
+------------------
+---- MONITORS ----
+------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-	output = "eDP-2",
-	mode = "highres",
-	position = "auto",
-	scale = "auto",
+    output   = "",
+    mode     = "preferred",
+    position = "auto",
+    scale    = 1,
 })
-hl.config({
-	xwayland = {
-		force_zero_scaling = true,
-	},
-})
+
+
+
