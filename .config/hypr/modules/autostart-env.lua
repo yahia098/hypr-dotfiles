09213c0cba21bd483f64c437f8fbf7d5e10544ca
yahia098 <yahiaoui0982@gmail.com>
2026-07-19 21:51:51 +0100

    made my submaps better

--- .config/hypr/modules/autostart-env.lua
+++ .config/hypr/modules/autostart-env.lua
@@ -6,6 +6,7 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("hyprpaper")
 	hl.exec_cmd("swaync")
 	hl.exec_cmd("$HOME/.config/waybar/scripts/launch.sh")
+	hl.exec_cmd("hypridle")
 end)
 os.execute("$HOME/.config/waybar/scripts/launch.sh")
 -------------------------------
